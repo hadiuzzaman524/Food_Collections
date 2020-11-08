908346bc1cd423f36a5c02a33ab2eae531f52191
Md. Hadiuzzaman <52348628+hadiuzzaman524@users.noreply.github.com>
2020-11-08 14:19:16 +0600

    design homepage

---
+++ food_collection/lib/screens/homepage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Food Collections'),
+          centerTitle: true,
+        ),
+        body: Container(),
+      ),
+    );
+  }
+}
